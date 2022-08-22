512e03b35c3f7d9d913e63af3e9ff36cbc8cfe84
KIMHYESU <kimss9209@gmail.com>
2022-08-23 00:12:55 +0900

    Complete: 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,69 +1,70 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Box from "./component/Box";
-import { FaRegHandRock } from "react-icons/fa";
 import { FaRegHandScissors } from "react-icons/fa";
+import { FaRegHandRock } from "react-icons/fa";
 import { FaRegHandPaper } from "react-icons/fa";
 
-function App() {
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "./images/바위이미지.jpg",
+  },
+
+  scissors: {
+    name: "Scissors",
+    img: "./images/가위이미지.png",
+  },
+
+  paper: {
+    name: "Paper",
+    img: "./images/보 이미지.jpg",
+  },
+};
+
+const App = () => {
   const [userSelect, setUserSelect] = useState(null);
   const [computerSelect, setComputerSelect] = useState(null);
-  const [result, setResult] = useState("");
+  const [userResult, setUserResult] = useState("");
   const [computerResult, setComputerResult] = useState("");
 
-  const choice = {
-    rock: {
-      name: "Rock",
-      img: "https://lh3.googleusercontent.com/qkVrT5AEoLRntKS_preFmH44T9GeOIv-lYT5RxwRI859U4Al3JKS3rwFEAAjHEw2gttpxRopcqcxn2BG0XcQy2lxao5gCFKWr0x1tKU=w600",
-    },
-    scissors: {
-      name: "Scissors",
-      img: "https://www.mouser.kr/images/marketingid/2020/img/177933245.png?v=031022.1142",
-    },
-    paper: {
-      name: "Paper",
-      img: "https://completesupplies.com.mt/wp-content/uploads/2020/10/46064.jpg",
-    },
-  };
-
   const play = (userChoice) => {
-    console.log("클릭", userChoice);
+    console.log("찍힘?", userChoice);
     setUserSelect(choice[userChoice]);
     let computerChoice = randomChoice();
     setComputerSelect(computerChoice);
-    setResult(judgement(choice[userChoice], computerChoice));
+    setUserResult(judgement(choice[userChoice], computerChoice));
     setComputerResult(judgement(computerChoice, choice[userChoice]));
   };
 
   const judgement = (user, computer) => {
     if (user.name == computer.name) {
-      return "TIE";
-    } else if (user.name == "Rock")
-      return computer.name == "Scissors" ? "WIN" : "LOSE";
-    else if (user.name == "Scissors")
-      return computer.name == "Paper" ? "WIN" : "LOSE";
+      return "Tie";
+    } else if (user.name == "Scissors")
+      return computer.name == "Paper" ? "Win" : "Lose";
+    else if (user.name == "Rock")
+      return computer.name == "Scissors" ? "Win" : "Lose";
     else if (user.name == "Paper")
-      return computer.name == "Rock" ? "WIN" : "LOSE";
+      return computer.name == "Rock" ? "Win" : "Lose";
   };
 
   const randomChoice = () => {
-    console.log("선택");
     let itemArray = Object.keys(choice);
     console.log("아이템어레이", itemArray);
     let randomItem = Math.floor(Math.random() * itemArray.length);
-    console.log("랜덤아이템", randomItem);
+    console.log("랜덤", randomItem);
     let final = itemArray[randomItem];
-    console.log("파이널", final);
+    console.log("보여줘!", final);
     return choice[final];
   };
 
   return (
-    <div>
+    <>
       <div className="outer">
-        <Box title="You" item={userSelect} result={result} />
+        <Box title="You" item={userSelect} result={userResult} />
         <Box title="Computer" item={computerSelect} result={computerResult} />
       </div>
-      <div className="buttons">
+      <div className="mainButtons">
         <button onClick={() => play("scissors")}>
           <FaRegHandScissors />
         </button>
@@ -74,8 +75,8 @@ function App() {
           <FaRegHandPaper />
         </button>
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 const Box = (props) => {
-  console.log("props", props);
   return (
     <div className={`box ${props.result}`}>
       <h1>{props.title}</h1>
       <h2>{props.item && props.item.name}</h2>
-      <img className="item-img" src={props.item && props.item.img} alt="img" />
+      <img
+        className="item-img"
+        src={props.item && props.item.img}
+        alt="images"
+      />
       <h1>{props.result}</h1>
     </div>
   );
