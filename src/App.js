a1d74965e8dcb9018d0793320890248dafc64048
KIMHYESU <kimss9209@gmail.com>
2022-08-22 17:17:24 +0900

    Modify: 기능 재구현 완료, react-icon 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Box from "./component/Box";
+import { FaRegHandRock } from "react-icons/fa";
+import { FaRegHandScissors } from "react-icons/fa";
+import { FaRegHandPaper } from "react-icons/fa";
 
 function App() {
   const [userSelect, setUserSelect] = useState(null);
   const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
+  const [computerResult, setComputerResult] = useState("");
 
   const choice = {
     rock: {
@@ -23,31 +28,51 @@ function App() {
 
   const play = (userChoice) => {
     console.log("클릭", userChoice);
-    setUserSelect(choice[userChoice], computerSelect);
+    setUserSelect(choice[userChoice]);
     let computerChoice = randomChoice();
-    setComputerSelect(computerChoice); // 컴퓨터도 랜덤한 이미지 출력 완
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
+    setComputerResult(judgement(computerChoice, choice[userChoice]));
   };
 
-  const randomChoice = (user, computer) => {
-    let itemArray = Object.keys(choice); // choice 객체의 key값 가져옴
-    console.log("배열은?", itemArray);
-    let randomItem = Math.floor(Math.random() * itemArray.length); // 가위바위보의 인덱스 번호(0,1,2)에 math.random(0.xxx) 곱해서 2.xxx 이런식으로 나오게끔 => 앞의숫자만 따오면 됨
-    console.log("랜덤밸류는", randomItem);
-    let final = itemArray[randomItem]; // 인덱스번호를 가위바위보(아이템 이름)로 가져옴
-    console.log("파이널?", final);
-    return choice[final]; // randomchoice()의 값이 computerChoice로 할당됨
+  const judgement = (user, computer) => {
+    if (user.name == computer.name) {
+      return "TIE";
+    } else if (user.name == "Rock")
+      return computer.name == "Scissors" ? "WIN" : "LOSE";
+    else if (user.name == "Scissors")
+      return computer.name == "Paper" ? "WIN" : "LOSE";
+    else if (user.name == "Paper")
+      return computer.name == "Rock" ? "WIN" : "LOSE";
+  };
+
+  const randomChoice = () => {
+    console.log("선택");
+    let itemArray = Object.keys(choice);
+    console.log("아이템어레이", itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    console.log("랜덤아이템", randomItem);
+    let final = itemArray[randomItem];
+    console.log("파이널", final);
+    return choice[final];
   };
 
   return (
     <div>
       <div className="outer">
-        <Box title="You" item={userSelect} />
-        <Box title="Computer" item={computerSelect} />
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={computerResult} />
       </div>
       <div className="buttons">
-        <button onClick={() => play("scissors")}>가위</button>
-        <button onClick={() => play("rock")}>바위</button>
-        <button onClick={() => play("paper")}>보</button>
+        <button onClick={() => play("scissors")}>
+          <FaRegHandScissors />
+        </button>
+        <button onClick={() => play("rock")}>
+          <FaRegHandRock />
+        </button>
+        <button onClick={() => play("paper")}>
+          <FaRegHandPaper />
+        </button>
       </div>
     </div>
   );
--- src/component/Box.js
+++ src/component/Box.js
@@ -3,11 +3,11 @@ import React from "react";
 const Box = (props) => {
   console.log("props", props);
   return (
-    <div className="box">
+    <div className={`box ${props.result}`}>
       <h1>{props.title}</h1>
       <h2>{props.item && props.item.name}</h2>
       <img className="item-img" src={props.item && props.item.img} alt="img" />
-      <h1>WIN</h1>
+      <h1>{props.result}</h1>
     </div>
   );
 };
